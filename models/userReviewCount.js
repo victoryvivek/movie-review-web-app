223c30b5ccab7270a4345114ab704cc887594259
konark1999 <kitugarg.1999@gmail.com>
2019-11-20 01:03:02 +0530

    movie review

---
+++ models/userReviewCount.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userReviewCountSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    reviewArray: {
+        type: Array,
+        of: String
+    },
+    ratingArray: {
+        type: Array, 
+        of: Number
+    },
+    imdbIdArray:{
+        type: Array, 
+        of: String
+    },
+    movieNameArray: {
+        type: Array
+        // of: String
+    },
+    dateArray:{
+        type: Array
+    }
+});
+
+module.exports = mongoose.model('UserReviewCountModel', userReviewCountSchema);
